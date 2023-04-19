767a007fec9119c26906a2d08594682bd6610873
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2023-04-19 20:31:32 +0300

    Final commit -> commments + coding style

--- assignments/1-marketplace/skel/tema/consumer.py
+++ assignments/1-marketplace/skel/tema/consumer.py
@@ -35,26 +35,34 @@ class Consumer(Thread):
         self.carts = carts
         self.marketplace = marketplace
         self.retry_wait_time = retry_wait_time
-        self.cart_id = marketplace.new_cart()
         Thread.__init__(self, **kwargs)
 
     def run(self):
+        # for every cart
         for cart in self.carts:
+            # get the current cart id
             cart_id = self.marketplace.new_cart()
 
+            # get operation to do in cart
             for operation in cart:
                 match operation["type"]:
                     case "add":
+                        # until we have quantity left
                         while operation["quantity"] > 0:
+                            # if we can add to cart
                             if self.marketplace.add_to_cart(cart_id, operation["product"]):
                                 operation["quantity"] -= 1
+                            # wait until we can retry and maybe the product will be provided
                             else:
                                 sleep(self.retry_wait_time)
                     case "remove":
+                        # until we have quantity left
                         while operation["quantity"] > 0:
                             self.marketplace.remove_from_cart(cart_id, operation["product"])
                             operation["quantity"] -= 1
 
+            # get the list of achieved products
             products = self.marketplace.place_order(cart_id)
+            # display the required message
             for product in products:
-                print("{} bought {}".format(self.name, product[1]))
+                print(f"{self.name} bought {product[1]}")
--- assignments/1-marketplace/skel/tema/producer.py
+++ assignments/1-marketplace/skel/tema/producer.py
@@ -34,17 +34,22 @@ class Producer(Thread):
         self.products = products
         self.marketplace = marketplace
         self.republish_wait_time = republish_wait_time
+        # get the current producer id
         self.producer_id = marketplace.register_producer()
+        # make the thread daemon
         Thread.__init__(self, daemon=kwargs["daemon"], kwargs=kwargs)
 
     def run(self):
         while True:
+            # for every product
             for product in self.products:
                 quantity = product[1]
+                # until it is no quantity to make
                 while quantity != 0:
+                    # if it is free space to make the product
                     if self.marketplace.publish(self.producer_id, product[0]):
                         time.sleep(product[2])
                         quantity -= 1
+                    # no free space, wait
                     else:
                         time.sleep(self.republish_wait_time)
-
